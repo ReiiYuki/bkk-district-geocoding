2523557178d53ca97334ab0d56cc0c885ac4c6f3
voraton.l <voraton@lmwn.com>
2025-06-08 16:26:39 +0700

    chore: fix build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { booleanPointInPolygon, point } from '@turf/turf'
-import geo from './districts.json'
+import geo from './districts.json' with { type: 'json' }
 
 function getDistrictFromLatLng(lat: number, lng: number) {
   const coordinate = point([lng, lat])
